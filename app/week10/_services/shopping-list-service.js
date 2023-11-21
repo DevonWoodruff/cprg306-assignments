ff0f0654056cb5dccd423a81c3c9a09efcc378c1
Arran Woodruff <arranwoodruff@gmail.com>
2023-11-20 22:02:11 -0700

    week 8 fixed and week 10 started

---
+++ app/week10/_services/shopping-list-service.js
@@ -0,0 +1,10 @@
+import { db } from "../_utils/firebase";
+import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+const getItems = () => {
+
+}
+
+const addItem = () => {
+  
+}
